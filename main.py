a8ac118af8c249e7c09b4f02a00dc5ef2757bc17
Chao <pkh_chao@photons.com.tw>
2024-05-14 14:10:36 +0800

    爬連結

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import aiohttp
 import asyncio
 import os
-from urllib.parse import urlparse
+import urllib.parse
+from playwright.sync_api import sync_playwright
+import requests
+import re
 
 def get_ts_url(base_url, ts_url):
     if ts_url.startswith('http'):
         return ts_url
 
     if ts_url.startswith('/'):
-        parsed_base_url = urlparse(base_url)
+        parsed_base_url = urllib.parse.urlparse(base_url)
         domain_with_scheme = parsed_base_url.scheme + '://' + parsed_base_url.netloc
         return domain_with_scheme + ts_url
 
@@ -48,6 +51,7 @@ async def download_m3u8_video(url, file_path):
 
             tasks = []
             for i, ts_url in enumerate(m3u8_list):
+                print('ts_url:',ts_url)
                 ts_url = get_ts_url(url, ts_url)
                 ts_file_path = file_path.rsplit('.', 1)[0] + f'_{i}.ts'
 
@@ -73,10 +77,65 @@ async def merge_ts_files(base_file_path, num_files):
             os.remove(ts_file_path)  # 删除临时的.ts文件
     print('所有ts文件合并完成')
 
+def check_content(url):
+    response = requests.get(url)
+    print(f"請求 URL: {url}, 狀態碼: {response.status_code}")
+
+    if response.status_code == 200:
+        content = response.text
+        m3u8_urls = re.findall(r'https?://[^\s]*\.m3u8', content)
+        if m3u8_urls:
+            print("找到的 .m3u8 檔案：")
+            for m3u8_url in m3u8_urls:
+                print(m3u8_url)
+                check_content(m3u8_url)
+        else:
+            ts_urls = re.findall(r'\b\w+\.ts\b', content)
+            if ts_urls:
+                print("找到正確的 .m3u8 路徑：",url)
+            else:
+                m3u8_paths = re.findall(r'(?<!\S)/?[^\s]+\.m3u8(?!\S)', content)
+                if m3u8_paths:
+                    print("找到的 .m3u8 字串：")
+                    for m3u8_path in m3u8_paths:
+                        print(m3u8_path)
+                        m3u8_url = get_ts_url(url, m3u8_path)
+                        print(m3u8_url)
+                        check_content(m3u8_url)
+                else:
+                    print("沒找到 content：", content)
+
+    else:
+        print(f"無法取得網絡資料，狀態碼: {response.status_code}")
+
+
+def getResponse(response):
+    if "m3u8" in response.url:
+        print("檢查m3u8 load url：",response.url)
+        check_content(response.url)
+
+
+def getm3u8():
+
+    # weburl = 'https://jable.tv/videos/sone-185/'
+    weburl = 'https://99kubo.cc/vod-199959734/play-ep1032.html'
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.on('response', getResponse)
+        page.goto(weburl)
+        page.wait_for_load_state('load')
+        browser.close()
+
+
 if __name__ == '__main__':
     # url = 'https://fuaf-uying.mushroomtrack.com/hls/AjToHMToKg1OJpr40BMizg/1715636791/24000/24397/24397.m3u8'
-    url = 'https://ikcdn01.ikzybf.com/20221018/Drqu7QAg/2000kb/hls/index.m3u8'
+    # url = 'https://ikcdn01.ikzybf.com/20221018/Drqu7QAg/2000kb/hls/index.m3u8'
+    url = 'https://ikcdn01.ikzybf.com/20221018/Drqu7QAg/index.m3u8'
     ts_file_path = 'dw/288.ts'
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(download_m3u8_video(url, ts_file_path))
+    # getm3u8()
+
